db726fcaaa3dd948e5fc0661f936d2aa51daf29f
sab-123 <74041851+sab-123@users.noreply.github.com>
2022-04-03 14:17:30 +0530

    Update addition.c

--- 3_Implementation/Functions/addition.c
+++ 3_Implementation/Functions/addition.c
@@ -1,1 +1,16 @@
+#include"calculator.h"
+int addition()
+{
+    int i, sum = 0, num, f_num; // declare a local variable
+    printf (" How many numbers you want to add: ");
+    scanf ("%d", &num);
+    printf (" Enter the numbers: \n ");
+    for (i = 1; i <= num; i++)
+    {
+        scanf(" %d", &f_num);
+        sum = sum + f_num;
+    }
+    printf (" Total Sum of the numbers = %d", sum);
+    return 0;
+}
 
