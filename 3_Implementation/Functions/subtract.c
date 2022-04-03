5a71cacb54a020f077da0405eff90cfde824c982
sab-123 <74041851+sab-123@users.noreply.github.com>
2022-04-03 14:19:09 +0530

    Update subtract.c

--- 3_Implementation/Functions/subtract.c
+++ 3_Implementation/Functions/subtract.c
@@ -1,1 +1,12 @@
+#include"calculator.h"
+int subtract()
+{
+    int n1, n2, res;
+    printf (" The first number is: ");
+    scanf ("  %d", &n1);
+    printf (" The second number is: ");
+    scanf ("  %d", &n2);
+    res = n1 - n2;
+    printf (" The subtraction of %d - %d is: %d", n1, n2, res);
+}
 
