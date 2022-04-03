64701720a3c65099b5644d593b6102490462505e
sab-123 <74041851+sab-123@users.noreply.github.com>
2022-04-03 14:20:18 +0530

    Update multiply.c

--- 3_Implementation/Functions/multiply.c
+++ 3_Implementation/Functions/multiply.c
@@ -1,1 +1,12 @@
+#include"calculator.h"
+int multiply()
+{
+    int n1, n2, res;
+    printf (" The first number is: ");
+    scanf ("  %d", &n1);
+    printf (" The second number is: ");
+    scanf ("  %d", &n2);
+    res = n1 * n2;
+    printf (" The multiply of %d * %d is: %d", n1, n2, res);
+}
 
