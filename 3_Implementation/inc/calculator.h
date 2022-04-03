fe738d49d29ec556ea07f0ce4e0d4150af0ed3c1
sab-123 <74041851+sab-123@users.noreply.github.com>
2022-04-03 14:24:37 +0530

    Create calculator.h

---
+++ 3_Implementation/inc/calculator.h
@@ -0,0 +1,18 @@
+#ifndef CALCULATOR_H_INCLUDED
+#define CALCULATOR_H_INCLUDED
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <stdlib.h>
+
+// function declarations
+int addition();
+int subtract();
+int multiply();
+int divide();
+int sqr();
+void exit();
+
+
+
+#endif // CALCULATOR_H_INCLUDED
